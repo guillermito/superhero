8c6100f7701907984480377de8e059a8b71a7bcf
dadapowell <jasonlalor@gmail.com>
2016-04-21 02:04:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
-var hero = {
-  spiderman: { 
-      name1:value1, 
-      name2:value2
-    }
- };
-
-
-$('button').on('click', function(){
-  // This variable stores the value of your input field
-  var input = $('input').val();
+$(document).ready(function(){
   
-});
+  var hero = {
+    spiderman: { 
+        name1:"value1", 
+        name2:"value2"
+      }
+   };
+
+  $('button').on('click', function(){
+    // This variable stores the value of your input field
+    var input = $('input').val();
+    
+  });
+  
+  
+  
+  // ***************************** 
+  // NOTES
+  // *****************************
+  // Remember append()?
+  $('body').append('<header><h2></h2></header>');
+  
+  // Meet text()!
+  // This will add text to HTML elements; it will replace any text that's already there
+  $('h2').text("Find your hero!");
+  
+  // Hint: Use .append() and/or .text() in your on('click') function!
+  
+  // *****************************
+  // Getting values from objects
+  /*
+  var car = {make:"Volkswagen", model:"Beetle", color:"red"};
+  console.log(car.color);
+  */
+  
+  // *****************************
+  // For..in loops
+  /*
+  for (var name in object) {
+    console.log(object[name]);
+  }
+  */
 
 
-
-// ***************************** 
-// NOTES
-// *****************************
-// Remember append()?
-$('header').append('<h2></h2>');
-
-// Meet text()!
-// This will add text to HTML elements; it will replace any text that's already there
-$('h2').text("Find your hero!");
-
-// Hint: Use .append() and/or .text() in your on('click') function!
-
-// *****************************
-// Getting values from objects
-/*
-var car = {make:"Volkswagen", model:"Beetle", color:"red"};
-console.log(car.color);
-*/
-
-// *****************************
-// For..in loops
-/*
-for (var name in object) {
-  console.log(object[name]);
-}
-*/
+})
